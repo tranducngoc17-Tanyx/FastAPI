251aa75eb4cbf3543a82c1094df023639f845fd2
Trần  Đức Ngọc <tranducngoc171717@gmail.com>
2026-06-29 10:20:42 +0700

    'ss3'

---
+++ ss3/btth2/main.py
@@ -0,0 +1,65 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+books = [
+    {
+        "id": 1,
+        "title": "Python Basic",
+        "author": "Lê Minh Thu",
+        "category": "programming",
+        "year": 2022,
+        "is_available": True
+    },
+    {
+        "id": 2,
+        "title": "Web API Design",
+        "author": "Phạm Lan Hồng",
+        "category": "web",
+        "year": 2021,
+        "is_available": False
+    },
+    {
+        "id": 3,
+        "title": "Database System",
+        "author": "Lê Minh Huyền",
+        "category": "database",
+        "year": 2020,
+        "is_available": True
+    },
+    {
+        "id": 4,
+        "title": "Clean Code",
+        "author": "Lê Ánh Linh",
+        "category": "programming",
+        "year": 2008,
+        "is_available": False
+    },
+    {
+        "id": 5,
+        "title": "Computer Network",
+        "author": "Vũ Hồng Vân",
+        "category": "network",
+        "year": 2019,
+        "is_available": True
+    }
+]
+
+@app.get("/books/available")
+def get_books_avai():
+    avai_books = []
+
+    for book in books:
+        if book["is_available"] == True:
+            avai_books.append(book)
+
+    return avai_books
+
+@app.get("/books/borrowed")
+def get_books_borrowed():
+    borrowed_books = []
+
+    for book in books:
+        if book["is_available"] == False:
+            borrowed_books.append(book)
+
+    return borrowed_books
